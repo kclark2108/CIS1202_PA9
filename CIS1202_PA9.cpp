7101e3c3b3ff230b2356731a9996b0f4dda4312b
kclark2108 <Kenneth.Clark@sinclair.edu>
2024-03-07 16:50:04 -0500

    Add project files.

---
+++ CIS1202_PA9.cpp
@@ -0,0 +1,12 @@
+// CIS1202_PA9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Mr. Clark Example
+// 3/7/24
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
